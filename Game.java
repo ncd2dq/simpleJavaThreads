4d3f5c44175cc445ef76b51f732a764d482e2246
ncd2dq <ncd2dq@virginia.edu>
2019-06-11 15:36:57 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -25,8 +25,12 @@ Create a class that implements the Runnable interface which only has 1
 public abstarct method - run()
 */
 class KeyBoardListener implements Runnable {
-  private boolean isListening = true;
+  /**
+  1) Please ensure that boolean variable which is used to stop the thread is volatile, otherwise, in the worst case your thread may not stop and run infinitely, Why? because, in the absence of any synchronization instruction e.g. volatile modifier here, the compiler is free to cache the value of boolean variable exit, which means even if the main thread makes it true, Server will always see it false, thus running infinitely. This concept is often tested in Java interviews as well.
+  */
+  private volatile boolean isListening = true;
   private Scanner keyboard;
+  private String killWord = "secret";
 
   public KeyBoardListener(Scanner scanner) {
     this.keyboard = scanner;
@@ -34,13 +38,18 @@ class KeyBoardListener implements Runnable {
 
   @Override
   public void run() {
+    // This is the main entry method to this separate threaded class
     while (this.isListening) {
+      // (Blocking) listening for keyboard inputs
       String myInput = keyboard.nextLine();
+      // Check if input is kill string
+      if (myInput.equals(this.killWord)) this.kill();
       System.out.println("Input: " + myInput);
     }
   }
 
   public void kill() {
+    // End this thread
     this.isListening = false;
   }
 }
